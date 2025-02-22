869e78ddb81eff05b4f683de5d5c3b21ef33e91f
nathabuddhi <nathabuddhi@gmail.com>
2025-02-22 12:41:21 +0700

    Initial Project Setup (maybe lol)

---
+++ Code/vortekia-backend/target/debug/vortekia-backend.d
@@ -0,0 +1,1 @@
+C:\Users\natha\SLC\TPA\ Desktop\DESKTOP-NB-242\code\vortekia-backend\target\debug\vortekia-backend.exe: C:\Users\natha\SLC\TPA\ Desktop\DESKTOP-NB-242\code\vortekia-backend\src\main.rs
